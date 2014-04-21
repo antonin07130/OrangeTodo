94dde0e32b82d12c30c4ee1886a1eac3bc53eb22
antonin07130 <antonin07130@gmail.com>
2014-04-21 02:43:18 +0200

    Working Dart Http client displaying JSON in a list. encoding and decoding of JSON is done by std Dart libraries.
    TODO : cleanup and comment

--- OrangeTodo/web/orangetodo.dart
+++ OrangeTodo/web/orangetodo.dart
@@ -1,5 +1,38 @@
 import 'dart:html';
+//import '../../OrangeTodoServer/Lib/TodoServer.dart';
 
+import 'dart:convert';
+
+
+UListElement todoList;
+ParagraphElement para;
 void main() {
+  todoList = querySelector('#todoList');
+  para = querySelector('#towrite');
+  new Element.tag('p');
+  getDataFromServer();
+}
 
+
+
+void getDataFromServer() {
+  var path = "ToDoListServerDB.db.json";
+  HttpRequest.getString("http://127.0.0.1:6060/")
+   .then(processString);
+  // .catchError(handleGetError);
+}
+
+void processString (String iString) {
+  para.text = iString;
+  // try to automatically decode input string... and put it in a list of maps
+  List<Map> listOfTodosMap = JSON.decode(iString);
+  for (Map itTodo in listOfTodosMap) {
+  //create a list element from the input
+  LIElement newListElement = new LIElement();
+  newListElement.text = itTodo["value"];
+  //add this new list element to the list container
+  todoList.append(newListElement);
+  para.text = "fuckin' A ! that's like a diode lighting up !";
+  }
+  
 }
