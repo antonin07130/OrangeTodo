c2a3861505166a5f9f7043b46c4b3e20b87d868d
antonin07130 <antonin07130@gmail.com>
2014-04-22 21:17:30 +0200

    futurized client

--- OrangeTodo/web/orangetodo.dart
+++ OrangeTodo/web/orangetodo.dart
@@ -1,38 +1,59 @@
 import 'dart:html';
 //import '../../OrangeTodoServer/Lib/TodoServer.dart';
-
+import 'dart:async';
 import 'dart:convert';
 
 
-UListElement todoList;
-ParagraphElement para;
+// elements linked to HTML form
+UListElement todoList; // List containing todos
+ParagraphElement statusText; // status text paragraph.
+
+// main routine
 void main() {
+  // create dart object from html elements using id identificator
   todoList = querySelector('#todoList');
-  para = querySelector('#towrite');
-  new Element.tag('p');
-  getDataFromServer();
+  statusText = querySelector('#statusText');
+  
+  statusText.text = "Loading contents ...";
+  // get list pf todos from the server
+  getTodosFromServer("http://127.0.0.1:6060/")
+  .then((todos){displayTodosInHTMLList(todos,todoList);})
+  .then((_){statusText.text = "Done";});
 }
 
 
-
-void getDataFromServer() {
-  var path = "ToDoListServerDB.db.json";
-  HttpRequest.getString("http://127.0.0.1:6060/")
-   .then(processString);
-  // .catchError(handleGetError);
+/**
+ * This function returns a Future<List<Map>> provided by the server
+ * It sends a GET request to specified URI.
+ */
+Future<List<Map>> getTodosFromServer(String iServerURL) {
+  Completer returnCompleter = new Completer();
+  
+  //var path = "ToDoListServerDB.db.json";
+  HttpRequest.getString(iServerURL)
+   .then((stringFromServer){returnCompleter.complete(processTodoListString(stringFromServer));});
+  return returnCompleter.future;
 }
 
-void processString (String iString) {
-  para.text = iString;
+
+Future<List<Map>> processTodoListString (String iJSONTodoList) {
+  Completer returnCompleter = new Completer();
   // try to automatically decode input string... and put it in a list of maps
-  List<Map> listOfTodosMap = JSON.decode(iString);
-  for (Map itTodo in listOfTodosMap) {
-  //create a list element from the input
-  LIElement newListElement = new LIElement();
-  newListElement.text = itTodo["value"];
-  //add this new list element to the list container
-  todoList.append(newListElement);
-  para.text = "fuckin' A ! that's like a diode lighting up !";
+  returnCompleter.complete(JSON.decode(iJSONTodoList));
+  return returnCompleter.future;
+}
+
+
+displayTodosInHTMLList(List<Map> todoList,UListElement displayHTMLList){
+  
+  for (Map itTodo in todoList) {
+    //create a list element from the input
+    LIElement newListElement = new LIElement();
+    newListElement.text = itTodo["value"];
+    //add this new list element to the list container
+    displayHTMLList.append(newListElement);
   }
+ 
+  
   
 }
